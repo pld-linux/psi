426eaf5f2bd295a62eddf1ca66152df25cac90dd
jpc <jpc@pld-linux.org>
2004-04-03 21:00:58 +0000

    - updated and renamed the rich roster patch - added a README
    Changed files:
        psi-richlistview.cpp -> 1.1.2.1
        psi-richlistview.h -> 1.1.2.1

--- psi-richlistview.cpp
+++ psi-richlistview.cpp
@@ -2,26 +2,70 @@
 #include <qsimplerichtext.h>
 #include <qpainter.h>
 #include <qpixmap.h>
+#include <qapplication.h>
+
 RichListViewItem::RichListViewItem( QListView * parent ):
 QListViewItem(parent)
 {
-//	setMultiLinesEnabled(true);	
+	v_rt = 0;
+	v_selected = false;
 }
 
 RichListViewItem::RichListViewItem( QListViewItem * parent ):
 QListViewItem(parent)
 {
-//	setMultiLinesEnabled(true);	
+	v_rt = 0;
+	v_selected = false;
 }
 
 RichListViewItem::~RichListViewItem()
 {
+	if(v_rt)
+		delete v_rt;
+}
+
+void RichListViewItem::setup()
+{
+	QListViewItem::setup();
+	int h = height();
+
+	QString txt = expandTemplate(0); // single column !!!
+	if( txt.isEmpty() ){
+		if(v_rt)
+			delete v_rt;
+		v_rt = 0;
+		return;
+	}
+    
+	const QListView* lv = listView();
+	const QPixmap* px = pixmap(0);
+	int left =  lv->itemMargin() + ((px)?(px->width() + lv->itemMargin()):0);
+
+	v_active = lv->isActiveWindow();
+	v_selected = isSelected();
+
+	if ( v_selected  ) {
+		txt = QString("<font color=\"%1\">").arg(listView()->colorGroup().color( QColorGroup::HighlightedText ).name()) + txt + "</font>";
+	}
+	
+	if(v_rt)
+		delete v_rt;
+	v_rt = new QSimpleRichText(txt, lv->font());
+	
+	v_rt->setWidth(lv->columnWidth(0) - left - depth() * lv->treeStepSize());
+
+	v_widthUsed = v_rt->widthUsed() + left;
+
+	h = QMAX( h, v_rt->height() );
+
+    if ( h % 2 > 0 )
+	h++;
+    setHeight( h );
 }
 
 void RichListViewItem::paintCell(QPainter *p, const QColorGroup &cg, int column, int width, int align)
 {
-	QString txt = expandTemplate(column);
-	if(txt.isEmpty()){
+	if(!v_rt){
 		QListViewItem::paintCell(p, cg, column, width, align);
 		return;
 	}
@@ -29,22 +73,22 @@ void RichListViewItem::paintCell(QPainter *p, const QColorGroup &cg, int column,
 	p->save();
 
 	QListView* lv = listView();
+
+	if ( isSelected() != v_selected || lv->isActiveWindow() != v_active) 
+		setup();
 	
 	int r = lv->itemMargin();
 	
 	const QBrush *paper;
-	int h = 0;
 	// setup (colors, sizes, ...)
 	if ( isSelected() ) {
 		paper = &cg.brush( QColorGroup::Highlight );
-		txt = QString("<font color=\"%1\">").arg(cg.color( QColorGroup::HighlightedText ).name()) + txt + "</font>";
 	}
 	else{
 		const QColorGroup::ColorRole crole = QPalette::backgroundRoleFromMode( lv->viewport()->backgroundMode() );
 		paper = &cg.brush( crole );
-		txt = txt;
 	}
-
+	
 	const QPixmap * px = pixmap( column );
 	QRect pxrect;
 	int pxw = 0;
@@ -52,36 +96,21 @@ void RichListViewItem::paintCell(QPainter *p, const QColorGroup &cg, int column,
 	if(px) {
 		pxw = px->width();
 		pxh = px->height();
-		h = QMAX(h, pxh + 2 * lv->itemMargin());
-		pxrect = QRect(r, (h - pxh)/2, pxw, pxh);
-		r += pxw + 1;
+		pxrect = QRect(r, (height() - pxh)/2, pxw, pxh);
+		r += pxw + lv->itemMargin();
 	}
 
-	QSimpleRichText rt(txt, lv->font());
-	rt.setWidth(width - r);
-	int rth = rt.height();
-	int rtw = v_widthUsed = rt.widthUsed();
-
-	h = QMAX(h, rth);
-
 	if(px)
-		pxrect.moveTop((h - pxh)/2);
+		pxrect.moveTop((height() - pxh)/2);
 
-	// make height odd number
-    if(h % 2 > 0)
-		h++;
-
-	if(height() != h)
-		setHeight(h);
-	
 	// start drawing
-	QRect rtrect(r, (h - rth)/2, rtw, rth);
-	rt.draw(p, rtrect.left(), rtrect.top(), rtrect, cg, paper);
+	QRect rtrect(r, (height() - v_rt->height())/2, v_widthUsed, v_rt->height());
+	v_rt->draw(p, rtrect.left(), rtrect.top(), rtrect, cg, paper);
 	
-	QRegion clip(0, 0, width, h);
+	QRegion clip(0, 0, width, height());
 	clip -= rtrect;
 	p->setClipRegion(clip, QPainter::CoordPainter);
-	p->fillRect( 0, 0, width, h, *paper );
+	p->fillRect( 0, 0, width, height(), *paper );
 
 	if(px)
 		p->drawPixmap(pxrect, *px);
