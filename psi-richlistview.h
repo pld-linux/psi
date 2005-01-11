ec5c9dd5976ded868b1ee1d9d61b1a767b1cdfcb
dzeus <dzeus@pld-linux.org>
2005-01-11 22:09:20 +0000

    - updated from DEVEL
    Changed files:
        psi-richlistview.h -> 1.3
        psi-roster-rich.patch -> 1.2

--- psi-richlistview.h
+++ psi-richlistview.h
@@ -2,7 +2,6 @@
 #define RICHLISTVIEW_H
 
 #include "qlistview.h"
-#include "common.h"
 class QSimpleRichText;
 class RichListViewItem : public QListViewItem  
 {
@@ -11,7 +10,6 @@ class RichListViewItem : public QListViewItem
 	QSimpleRichText* v_rt;
 protected:
 	virtual void paintCell( QPainter * p, const QColorGroup & cg, int column, int width, int align );	
-//	virtual QString expandTemplate(int column = 0) { return QString("<nobr>") + expandEntities(text(column)) + "</nobr>";};
 	virtual QString expandTemplate(int column = 0) { return QString::null;};
 public:
 	RichListViewItem( QListView * parent );
